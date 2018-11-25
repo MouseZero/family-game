0e186858b834418574df3efeaf4ec7232322e1c6
MouseZero <rm@mousezero.com>
2018-11-24 19:19:11 -0800

    code gets transpiled

--- src/main.js
+++ src/main.js
@@ -79,3 +79,11 @@ store.dispatch({
   direction: 'east'
 })
 
+import React from 'react'
+import ReactDom from 'react-dom'
+
+ReactDom.render((
+  <div>Hello World</div>
+), document.getElementById('app'))
+
+
